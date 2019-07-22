16750ace22430e8be6abbe9913ef8ad7300882b8
droll-accelerator-1483 <srsani@gmail.com>
2019-07-22 20:12:53 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  for i in array
+    yield (i)
+    new_array.append(yield (i))
+  end
+  return new_array
+end
+
+def reduce(source_array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    n= source_array[i]
+    new = yield( new, n)
+    i += 1
+  end
+  return new
+end
